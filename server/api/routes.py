0494563094356628105e62ac6337fed2fbedbf67
Hiresh Bremanand <hbremanand4@gmail.com>
2025-04-01 01:55:55 +0100

    updated readme, api routes for auth

--- server/api/routes.py
+++ server/api/routes.py
@@ -29,13 +29,17 @@ async def run_agent(request: Request):
     browserbase_key = data.get("browserbase_key")
     browserbase_project_id = data.get("browserbase_project_id")
     openai_key = data.get("openai_key")
+    contextId = data.get("contextId")
+    email = data.get("email")
+    password = data.get("password")
 
-    print(browserbase_key, browserbase_project_id, openai_key)
+    print(browserbase_key, browserbase_project_id, openai_key, contextId, email, password)
 
     # Setup browser and context
     browser, context, bb_session = await setup_browser(
         browserbase_key, 
-        browserbase_project_id
+        browserbase_project_id,
+        contextId
     )
 
     # Get session
@@ -44,6 +48,46 @@ async def run_agent(request: Request):
     try:
         # Setup agent
         agent = await setup_agent(browser, context, openai_key)
+
+        # Run agent
+        await agent.run()
+
+    finally: 
+        # Close browser
+        await browser.close()
+
+
+    print("Session: ", session)
+    print("BB Session: ", bb_session)
+
+    # Return session 
+    return bb_session
+
+@router.post("/login-x" , status_code=201)
+async def login_x(request: Request):
+    data = await request.json()
+    browserbase_key = data.get("browserbase_key")
+    browserbase_project_id = data.get("browserbase_project_id")
+    openai_key = data.get("openai_key")
+    contextId = data.get("contextId")
+    email = data.get("email")
+    password = data.get("password")
+
+    print(browserbase_key, browserbase_project_id, openai_key, contextId, email, password)
+
+    # Setup browser and context
+    browser, context, bb_session = await setup_browser(
+        browserbase_key, 
+        browserbase_project_id,
+        contextId
+    )
+
+    # Get session
+    session = await context.get_session()
+
+    try:
+        # Setup agent
+        agent = await setup_agent(browser, context, openai_key, email, password)
 
         # Run agent
         await agent.run()
